d14d5c0a53cc73608a901426f0581c50b118a110
Matt <matthew.whetton@yahoo.co.uk>
2015-03-15 09:34:54 +0000

    Fix up build file

--- build.bat
+++ build.bat
@@ -14,10 +14,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
  
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\SharpComicVine.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild SharpComicVine.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
  
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
  
-%nuget% pack "src\SharpComicVine\SharpComicVine.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "SharpComicVine\SharpComicVine.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
