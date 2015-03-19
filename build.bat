c2496462348104398512bf6ff979a95bfd156b7e
Matt <matthew.whetton@yahoo.co.uk>
2015-03-19 15:22:56 +0000

    Changed to semantic versioning inside bat file

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ if "%config%" == "" (
    set config=Release
 )
  
-set version=1.0.0
-if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
-)
+set version=1.0.5-pre
+REM if not "%PackageVersion%" == "" (
+REM    set version=%PackageVersion%
+REM )
  
 set nuget=
 if "%nuget%" == "" (
@@ -16,9 +16,9 @@ if "%nuget%" == "" (
  
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild SharpComicVine\SharpComicVine.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
  
-mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net40
+REM mkdir Build
+REM mkdir Build\lib
+REM mkdir Build\lib\net40
  
-%nuget% pack "SharpComicVine\SharpComicVine.csproj"
+%nuget% pack "SharpComicVine\SharpComicVine.csproj"  -p Configuration="%config%" -Version %version% 
 REM %nuget% pack "SharpComicVine\SharpComicVine.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
